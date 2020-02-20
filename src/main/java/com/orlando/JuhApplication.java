e073c1a7ad2d86a331fc16c75176c1e45ff17967
Fernanda KASTRUP <43493818+Kastrupf@users.noreply.github.com>
2020-02-20 19:53:47 +0100

    Configurations

---
+++ src/main/java/com/orlando/JuhApplication.java
@@ -0,0 +1,13 @@
+package com.orlando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuhApplication.class, args);
+    }
+
+}
